4869ba4922740ff0cabefb5e1badc4867c4ec70e
Vivek Anand Singh <17vivekanandsingh@gmail.com>
2025-11-11 19:10:02 +0530

    server side api call and rendering

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react'
+
+const AddToCart = () => {
+  return (
+    <div>
+        <button onClick={() => console.log('Clicked')}>Add to Card</button>
+    </div>
+  )
+}
+
+export default AddToCart;
---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import AddToCart from './AddToCart';
+const ProductCard = () => {
+  return (
+    <div>
+        <AddToCart/>
+    </div>
+  )
+}
+
+export default ProductCard;
